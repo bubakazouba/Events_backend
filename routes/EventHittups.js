ae8a34c3c53019d352916510ece8959c8905fb6f
Abdulrahman Sahmoud <ahsahmoud@ucdavis.edu>
2016-02-02 00:12:32 -0800

    removes more routes

--- routes/EventHittups.js
+++ routes/EventHittups.js
@@ -4,17 +4,6 @@ var router = express.Router();
 var EventHittups = require('../models/EventHittups');
 var HittupHelper = require('../modules/HittupHelper');
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('Hello /Hittup Events!');
-});
-
-router.post('/UnjoinHittup', function (req, res) {
-    HittupHelper.unjoin(EventHittups,req, function (result) {
-        res.send(result);
-    });
-});
-
 
 router.post('/RemoveHittup', function (req, res) {
     HittupHelper.remove(EventHittups,req, function (result) {
@@ -22,33 +11,8 @@ router.post('/RemoveHittup', function (req, res) {
     });
 });
 
-
-router.post('/GetHittup', function (req, res) {
-    HittupHelper.getEventHittup(req, function (result) {
-        res.send(result);
-    });
-});
-
 router.post('/GetAllHittups', function (req, res) {
     HittupHelper.getAllEventHittups(req, function (result) {
-        res.send(result);
-    });
-});
-
-router.post('/InviteFriends', function (req, res){
-    HittupHelper.invite(EventHittups, req, function (result){
-        res.send(result);
-    });
-});
-
-router.post('/UpdateHittup', function (req, res) {
-    HittupHelper.update(EventHittups, req, function (result) {
-        res.send(result);
-    });
-});
-
-router.post('/JoinHittup', function (req, res) {
-    HittupHelper.join(EventHittups, req, function (result) {
         res.send(result);
     });
 });
